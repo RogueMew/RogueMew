082b7694787cf5f9678df4ba79f06aedb43dc7d3
RogueMew <48804205+RogueMew@users.noreply.github.com>
2026-06-26 05:00:05 -0400

    new front page

--- sources/index.js
+++ sources/index.js
@@ -17,8 +17,59 @@ function detecDevice() {
   return device;
 }
 
-window.onload = function () {
-  if (device()) {
-    document.getElementById("headerBar").style.visibility = "hidden"
+function duplicateDeck(deckName, deckBoxName) {
+  const deck = document.getElementById(deckName);
+  const cloneDeck = deck.cloneNode(true);
+  console.log(deckBoxName);
+  document.getElementById(deckBoxName).appendChild(cloneDeck);
 }
+
+function populateDecks(listOfImages, deckName) {
+  listOfImages.forEach((imageName) => {
+    let imageBox = document.createElement("img");
+    imageBox.className = "card";
+    imageBox.src = imageName;
+    document.getElementById(deckName).appendChild(imageBox);
+  });
+}
+
+window.onload = function () {
+  if (detecDevice()) {
+    let elementsNeededHiding = document.getElementsByClassName("mobileHidden");
+    for (let index = 0; index < elementsNeededHiding.length; index++) {
+      const element = elementsNeededHiding[index];
+      element.computedStyleMap.visibility = "hidden";
+    }
+  } else {
+    let deck1Images = [
+      "./sources/images/IMG_7450.jpg",
+      "./sources/images/IMG_7515.jpg",
+      "./sources/images/IMG_7851.jpg",
+      "./sources/images/IMG_7899.jpg",
+      "./sources/images/IMG_7985.jpg",
+      "./sources/images/IMG_8013.jpg",
+      "./sources/images/IMG_8051.jpg",
+      "./sources/images/IMG_8055.jpg",
+      "./sources/images/IMG_8080.jpg",
+    ];
+
+    let deck2Images = [
+      "./sources/images/IMG_7175.jpg",
+      "./sources/images/IMG_7197.jpg",
+      "./sources/images/IMG_7246.jpg",
+      "./sources/images/IMG_7072.jpg",
+      "./sources/images/IMG_7073.jpg",
+      "./sources/images/IMG_7074.jpg",
+      "./sources/images/IMG_7081.jpg",
+      "./sources/images/IMG_7082.jpg",
+      "./sources/images/IMG_7146.jpg",
+    ];
+
+    populateDecks(deck1Images, "deck1");
+    duplicateDeck("deck1", "deckBox1");
+
+    populateDecks(deck2Images, "deck2");
+    duplicateDeck("deck2", "deckBox2");
+
+  }
 };
